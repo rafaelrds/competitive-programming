fa650556ea1778dee0a557721de80288a53109de
rafaelrds <crafards@gmail.com>
2017-10-08 00:55:09 -0300

    Added a simple square matrix problem

--- uri/1435.cpp
+++ uri/1435.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-int main() {
-	int N;
-	cin >> N;
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-
+void solve(int N)
+{
+	int matrix[N][N];
+	for (size_t row = 0; row < N; row++) {
+		for (size_t col = 0; col < N; col++) {
+			matrix[row][col] = min(N - row, min(N - col, min(row + 1, col + 1)));
 		}
+	}
+	for (size_t row = 0; row < N; row++) {
+		for (size_t col = 0; col < N; col++) {
+			printf("%3d", matrix[row][col]);
+			if (col != N - 1) cout << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int main () {
+	int N;
+	while (true) {
+		cin >> N;
+		if (N == 0) break;
+		solve(N);
 	}
 
 	return 0;
