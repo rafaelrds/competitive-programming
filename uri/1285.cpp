6ce929901ab215c49cf2e893f6f71efe739fd7be
rafaelrds <crafards@gmail.com>
2017-10-11 23:07:10 -0300

    One pace at 1285

--- uri/1285.cpp
+++ uri/1285.cpp
@@ -7,30 +7,25 @@ using namespace std;
 int main ()
 {
 	int N, M, ans = 0;
-	while (scanf("%d %d", &N, &M) != EOF)
-	{
+	while (scanf("%d %d", &N, &M) != EOF) {
 		int i, ans = 0;
-		for (i=N; i <= M; i++)
-		{
+		for (i=N; i <= M; i++) {
 			int numbers[10];
 			memset(numbers, 0, sizeof(numbers));
 			int number = i;
-			while (number != 0)
-			{
+			bool flag = true;
+			while (number != 0)	{
 				int a = number % 10;
-				numbers[a]++;
+				if (++numbers[a] > 1) {
+					flag = false;
+					break;
+				}
 				number -= a, number /= 10;
 			}
-
-			bool flag = true;
-			for (int j = 0; j < 10; j++)
-				if (numbers[j] > 1)
-					flag = false;
-			
 			if (flag)
 				ans++;
 		}
-		
+
 		cout << ans << endl;
 
 		ans = 0;
