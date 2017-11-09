e54b0d8e1e9d5f408bf2eb62df54ae5d8a9176bb
rafaelrds <crafards@gmail.com>
2017-11-09 01:14:55 -0300

    Learned to reference a string in instead of passing by value to a function

--- codeforces/95A_2.cpp
+++ codeforces/95A_2.cpp
@@ -4,12 +4,11 @@
 
 using namespace std;
 
-string changeCase(string s) {
+void changeCase(string& s) {
     for (int i = 0; i < s.size(); i++) {
         if (islower(s[i])) s[i] = toupper(s[i]);
         else s[i] = tolower(s[i]);
     }
-    return s;
 }
 
 bool isCaps(string s) {
@@ -19,17 +18,16 @@ bool isCaps(string s) {
     return true;
 }
 
-string solve(string s) {
+void solve(string& s) {
     if (isCaps(s))
-        s = changeCase(s);
-    return s;
+        changeCase(s);
 }   
 
 int main() {
     string s;
     cin >> s;
 
-    s = solve(s);
+    solve(s);
     cout << s << endl;
 
     return 0;
